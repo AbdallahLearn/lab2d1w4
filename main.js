9a91b300514f7773e5088623fea5f009fb6a54e9
AbdallahLearn <abdallahjhn1@gmail.com>
2024-09-15 13:19:04 +0300

    edit

--- main.js
+++ main.js
@@ -9,16 +9,16 @@ fetch(url)
         container.appendChild(img);
 
         function changeImage() {
-            fetch(url) // Fetch a new image from the API
+            fetch(url) 
                 .then((res) => res.json())
                 .then((data) => {
-                    img.style.opacity = 0; // Fade out
+                    img.style.opacity = 0;
                     setTimeout(() => {
-                        img.src = data.file; // Change the image source
-                        img.style.opacity = 1; // Fade in
-                    }, 2000); // Match this duration with the CSS transition
+                        img.src = data.file; 
+                        img.style.opacity = 1; 
+                    }, 2000); 
                 });
         }
 
-        setInterval(changeImage, 3000); // Change image every 2 seconds
+        setInterval(changeImage, 3000);
     });
